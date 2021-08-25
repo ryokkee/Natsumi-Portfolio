4fb3a3cc3da620abc70b1a7243651c8e11362586
ryokkee <73788538+ryokkee@users.noreply.github.com>
2021-08-25 22:57:59 +0900

    8月25日

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('scroll', function () {
 	for (let i = 0; i < targetElement.length; i++) {
 		const getElementDistance =
 			targetElement[i].getBoundingClientRect().top +
-			targetElement[i].clientHeight * 0.6; //６割くらい見えたらshow 0.6
+			targetElement[i].clientHeight * 0.3; //６割くらい見えたらshow 0.6
 		//二つ目が見えたらshowというクラスを足す
 		if (window.innerHeight > getElementDistance) {
 			targetElement[i].classList.add('show');
